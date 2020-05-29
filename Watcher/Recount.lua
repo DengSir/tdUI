7ce634a4ae746df5792323741960ff93706b70c0
Dencer <tdaddon@163.com>
2020-05-29 15:43:43 +0800

    update

--- Watcher/Recount.lua
+++ Watcher/Recount.lua
@@ -47,34 +47,90 @@ ns.addon('Recount', function()
     do
         Window:SetBackdrop(nil)
 
+        local SM = LibStub:GetLibrary('LibSharedMedia-3.0')
+
         local WIDTH = 240
         local HEIGHT
 
-        -- local function SetFont(fontString, font, size, outline)
-        --     fontString:origSetFont(font, 12, 'OUTLINE')
-        -- end
+        local function GetBarTexture()
+            return SM:Fetch(SM.MediaType.STATUSBAR, Recount.db.profile.BarTexture)
+        end
+
+        local function UpdateFont(label)
+            local font = SM:Fetch('font', Recount.db.profile.BarFont)
+            label:origSetFont(font, Recount.db.profile.BarFontSize)
+        end
+
+        local function UpdateBarBackground(row, texture)
+            row.bg:SetTexture(texture or GetBarTexture())
+        end
+
+        local function UpdateBarFonts()
+            for _, row in pairs(Window.Rows) do
+                UpdateFont(row.LeftText)
+                UpdateFont(row.RightText)
+            end
+        end
+
+        local function UpdateBarTextures()
+            local texture = GetBarTexture()
+            for _, row in pairs(Window.Rows) do
+                UpdateBarBackground(row, texture)
+            end
+        end
+
+        local function UpdateBarIcons()
+            for _, row in pairs(Window.Rows) do
+                if row:IsVisible() then
+                    local class = row.TooltipData and row.TooltipData.enClass
+                    if class then
+                        row.icon:SetTexture([[Interface\Glues\CharacterCreate\ui-charactercreate-classes]])
+                        row.icon:SetTexCoord(unpack(CLASS_ICON_TCOORDS[class]))
+                    else
+                        row.icon:SetTexture([[Interface\Icons\ability_dualwield]])
+                        row.icon:SetTexCoord(0, 1, 0, 1)
+                    end
+                end
+            end
+        end
 
         local function HookFontString(fontString)
-            -- fontString.origSetFont = fontString.SetFont
-            fontString:SetFont(ChatFontNormal:GetFont(), 13, 'OUTLINE')
-            fontString:SetAlpha(0.6)
-            fontString.SetFont = nop
-            -- SetFont(fontString, fontString:GetFont())
+            fontString.origSetFont = fontString.SetFont
+            fontString.SetFont = UpdateFont
+            UpdateFont(fontString)
+        end
+
+        local function RowOnSizeChanged(row, width, height)
+            row.icon:SetSize(height, height)
         end
 
         local function SetupRow(_, row)
+            row.icon = row:CreateTexture(nil, 'ARTWORK')
+            row.icon:SetPoint('BOTTOMLEFT')
+
             row.StatusBar:ClearAllPoints()
-            row.StatusBar:SetPoint('BOTTOMLEFT')
+            row.StatusBar:SetPoint('BOTTOMLEFT', row.icon, 'BOTTOMRIGHT')
             row.StatusBar:SetPoint('BOTTOMRIGHT')
             row.StatusBar:SetHeight(8)
 
+            row.bg = row.StatusBar:CreateTexture(nil, 'BACKGROUND')
+            row.bg:SetAllPoints(row.StatusBar)
+            row.bg:SetHorizTile(false)
+            row.bg:SetVertTile(false)
+            row.bg:SetVertexColor(0, 0, 0, 0.3)
+
             row.LeftText:ClearAllPoints()
-            row.LeftText:SetPoint('BOTTOMLEFT', row.StatusBar, 'BOTTOMLEFT', 0, 2)
+            row.LeftText:SetPoint('LEFT', row.icon, 'RIGHT', 0, 0)
             HookFontString(row.LeftText)
 
             row.RightText:ClearAllPoints()
-            row.RightText:SetPoint('BOTTOMRIGHT', row.StatusBar, 'BOTTOMRIGHT', 0, 2)
+            row.RightText:SetPoint('RIGHT', row, 'RIGHT', 0, 0)
             HookFontString(row.RightText)
+
+            row:HookScript('OnSizeChanged', RowOnSizeChanged)
+
+            UpdateBarBackground(row)
+            RowOnSizeChanged(row, row:GetSize())
         end
 
         local function LockMainWindow()
@@ -101,7 +157,7 @@ ns.addon('Recount', function()
             end
         end
 
-        for _, v in pairs(Recount.MainWindow.Rows) do
+        for _, v in pairs(Window.Rows) do
             SetupRow(nil, v)
         end
 
@@ -110,23 +166,76 @@ ns.addon('Recount', function()
 
         LockMainWindow()
         ScaleMainWindow()
-        C_Timer.After(1, UpdateMainWindowSize)
+        -- UpdateBarTextures()
+        -- UpdateBarFonts()
 
         ns.securehook(Recount, 'SetupBar', SetupRow)
         ns.securehook(Recount, 'BarsChanged', UpdateMainWindowSize)
         ns.securehook(Recount, 'LockWindows', LockMainWindow)
         ns.securehook(Recount, 'ScaleWindows', ScaleMainWindow)
+        ns.securehook(Recount, 'UpdateBarTextures', UpdateBarTextures)
+        ns.securehook(Recount, 'SetBarTextures', UpdateBarTextures)
+        ns.securehook(Recount, 'RefreshMainWindow', function()
+            UpdateBarIcons()
+            UpdateMainWindowSize()
+        end)
         ns.securehook(Recount, 'ResetPositionAllWindows', function()
             ns.WatchManager:UpdateFrames()
         end)
 
-        ns.securehook(Recount, 'RefreshMainWindow', function()
-            UpdateMainWindowSize()
-        end)
-
         do -- options
             Recount.db.profile.BarTextColorSwap = false
+            Recount.db.profile.Font = nil
+            Recount.db.profile.BarFontSize = 13
+        end
 
+        do
+            local FontOptions = Recount_Config_Fonts_Scrollbar:GetParent()
+
+            local slider = CreateFrame('Slider', 'Recount_ConfigWindow_BarFontSize', FontOptions,
+                                       'OptionsSliderTemplate')
+
+            slider:SetOrientation('HORIZONTAL')
+            slider:SetMinMaxValues(6, 35)
+            slider:SetValueStep(0.5)
+            slider:SetObeyStepOnDrag(true)
+            slider:SetWidth(180)
+            slider:SetHeight(16)
+            slider:SetPoint('TOP', FontOptions, 'TOP', 0, -220)
+            slider:SetScript('OnValueChanged', function(self)
+                Recount.db.profile.BarFontSize = self:GetValue()
+                _G[self:GetName() .. 'Text']:SetText('Font Size' .. ': ' .. Recount.db.profile.BarFontSize)
+                UpdateBarFonts()
+            end)
+            slider:SetScript('OnMouseWheel', function(self, delta)
+                self:SetValue(self:GetValue() + (delta * self:GetValueStep()))
+            end)
+            _G[slider:GetName() .. 'High']:SetText('36')
+            _G[slider:GetName() .. 'Low']:SetText('6')
+            slider:SetValue(Recount.db.profile.BarFontSize)
+
+            local function UpdateOptionFonts()
+                for _, row in pairs(FontOptions.Rows) do
+                    if row.SetTo == Recount.db.profile.BarFont then
+                        row.Texture:SetVertexColor(0.2, 0.9, 0.2)
+                    else
+                        row.Texture:SetVertexColor(0.9, 0.2, 0.2)
+                    end
+                end
+            end
+
+            UpdateOptionFonts()
+
+            function Recount:SetFont(fontname)
+                Recount.db.profile.BarFont = fontname
+                UpdateBarFonts()
+            end
+
+            for _, row in ipairs(FontOptions.Rows) do
+                row:HookScript('OnMouseDown', UpdateOptionFonts)
+            end
+
+            Recount_Config_Fonts_Scrollbar:HookScript('OnVerticalScroll', UpdateOptionFonts)
         end
 
         do -- title
