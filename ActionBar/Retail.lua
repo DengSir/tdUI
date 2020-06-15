d2ea94c90ecc72d4f21ff729dfcf7a0b825e0ffb
Dencer <tdaddon@163.com>
2020-06-15 13:40:28 +0800

    update

--- ActionBar/Retail.lua
+++ ActionBar/Retail.lua
@@ -6,79 +6,71 @@
 ---@type ns
 local ns = select(2, ...)
 
+local LARGE_WIDTH = select(2, GetAtlasInfo('hud-MainMenuBar-large'))
+local SMALL_WIDTH = select(2, GetAtlasInfo('hud-MainMenuBar-small'))
+
 ---@type Frame
-local MainMenuBar, MainMenuExpBar, ReputationWatchBar, MainMenuBarArtFrame = MainMenuBar, MainMenuExpBar,
-                                                                             ReputationWatchBar, MainMenuBarArtFrame
+local MainMenuBar, MainMenuExpBar, ReputationWatchBar, MainMenuBarArtFrame, MultiBarBottomRight = MainMenuBar,
+                                                                                                  MainMenuExpBar,
+                                                                                                  ReputationWatchBar,
+                                                                                                  MainMenuBarArtFrame,
+                                                                                                  MultiBarBottomRight
 
 local Hider = CreateFrame('Frame')
 Hider:Hide()
 
-local function Hide(frame)
+local function hide(frame)
     frame:Hide()
     frame:SetParent(Hider)
 end
 
-Hide(MainMenuBarTexture0)
-Hide(MainMenuBarTexture1)
-Hide(MainMenuBarTexture2)
-Hide(MainMenuBarTexture3)
+local function point(frame, ...)
+    frame:ClearAllPoints()
+    frame:SetPoint(...)
+end
 
-Hide(MainMenuXPBarTexture0)
-Hide(MainMenuXPBarTexture1)
-Hide(MainMenuXPBarTexture2)
-Hide(MainMenuXPBarTexture3)
+hide(MainMenuBarTexture0)
+hide(MainMenuBarTexture1)
+hide(MainMenuBarTexture2)
+hide(MainMenuBarTexture3)
 
-Hide(MainMenuBarMaxLevelBar)
-Hide(MainMenuBarPerformanceBarFrame)
+hide(MainMenuXPBarTexture0)
+hide(MainMenuXPBarTexture1)
+hide(MainMenuXPBarTexture2)
+hide(MainMenuXPBarTexture3)
 
-Hide(ReputationWatchBar.StatusBar.WatchBarTexture0)
-Hide(ReputationWatchBar.StatusBar.WatchBarTexture1)
-Hide(ReputationWatchBar.StatusBar.WatchBarTexture2)
-Hide(ReputationWatchBar.StatusBar.WatchBarTexture3)
+hide(MainMenuBarMaxLevelBar)
+hide(MainMenuBarPerformanceBarFrame)
 
-Hide(ReputationWatchBar.StatusBar.XPBarTexture0)
-Hide(ReputationWatchBar.StatusBar.XPBarTexture1)
-Hide(ReputationWatchBar.StatusBar.XPBarTexture2)
-Hide(ReputationWatchBar.StatusBar.XPBarTexture3)
+hide(ReputationWatchBar.StatusBar.WatchBarTexture0)
+hide(ReputationWatchBar.StatusBar.WatchBarTexture1)
+hide(ReputationWatchBar.StatusBar.WatchBarTexture2)
+hide(ReputationWatchBar.StatusBar.WatchBarTexture3)
 
-CharacterMicroButton:ClearAllPoints()
-CharacterMicroButton:SetPoint('BOTTOMLEFT', UIParent, 'BOTTOMLEFT')
+hide(ReputationWatchBar.StatusBar.XPBarTexture0)
+hide(ReputationWatchBar.StatusBar.XPBarTexture1)
+hide(ReputationWatchBar.StatusBar.XPBarTexture2)
+hide(ReputationWatchBar.StatusBar.XPBarTexture3)
 
-MultiBarBottomLeftButton1:ClearAllPoints()
-MultiBarBottomLeftButton1:SetPoint('BOTTOMLEFT', ActionButton1, 'TOPLEFT', 0, 13)
+point(MultiBarBottomLeftButton1, 'BOTTOMLEFT', ActionButton1, 'TOPLEFT', 0, 13)
+point(MultiBarBottomRightButton1, 'BOTTOMLEFT', ActionButton12, 'BOTTOMRIGHT', 45, 0)
+point(MultiBarBottomRightButton7, 'BOTTOMLEFT', MultiBarBottomRightButton1, 'TOPLEFT', 0, 13)
 
-MultiBarBottomRightButton1:ClearAllPoints()
-MultiBarBottomRightButton1:SetPoint('BOTTOMLEFT', ActionButton12, 'BOTTOMRIGHT', 45, 0)
+point(ActionBarUpButton, 'CENTER', MainMenuBarArtFrame, 'BOTTOMLEFT', 522, 31)
+point(ActionBarDownButton, 'CENTER', MainMenuBarArtFrame, 'BOTTOMLEFT', 522, 11)
 
-MultiBarBottomRightButton7:ClearAllPoints()
-MultiBarBottomRightButton7:SetPoint('BOTTOMLEFT', MultiBarBottomRightButton1, 'TOPLEFT', 0, 13)
+point(MainMenuBarPageNumber, 'CENTER', MainMenuBarArtFrame, 'BOTTOMLEFT', 540, 53 / 2 - 5)
+point(MainMenuBarLeftEndCap, 'BOTTOM', MainMenuBar, 'BOTTOMLEFT', -32, 0)
+point(MainMenuBarRightEndCap, 'BOTTOM', MainMenuBar, 'BOTTOMRIGHT', 32, 0)
+point(StanceBarLeft, 'BOTTOMLEFT', StanceButton1, 'BOTTOMLEFT', -11, -3)
+point(SlidingActionBarTexture0, 'LEFT', PetActionButton1, 'LEFT', -36, 1)
 
-StanceButton1:ClearAllPoints()
-StanceButton1:SetPoint('BOTTOMLEFT', ActionButton1, 'TOPLEFT', 33, 11)
+point(MainMenuExpBar, 'BOTTOM', MainMenuBar, 'BOTTOM')
+point(MainMenuBarExpText, 'CENTER', MainMenuExpBar, 'CENTER')
 
-ActionBarUpButton:ClearAllPoints()
-ActionBarUpButton:SetPoint('CENTER', MainMenuBarArtFrame, 'BOTTOMLEFT', 522, 31)
-
-ActionBarDownButton:ClearAllPoints()
-ActionBarDownButton:SetPoint('CENTER', MainMenuBarArtFrame, 'BOTTOMLEFT', 522, 11)
-
-MainMenuBarPageNumber:ClearAllPoints()
-MainMenuBarPageNumber:SetPoint('CENTER', MainMenuBarArtFrame, 'BOTTOMLEFT', 540, 53 / 2 - 5)
-
-MainMenuBarLeftEndCap:ClearAllPoints()
-MainMenuBarLeftEndCap:SetPoint('BOTTOM', MainMenuBar, 'BOTTOMLEFT', -32, 0)
-
-MainMenuBarRightEndCap:ClearAllPoints()
-MainMenuBarRightEndCap:SetPoint('BOTTOM', MainMenuBar, 'BOTTOMRIGHT', 32, 0)
-
-StanceBarLeft:ClearAllPoints()
-StanceBarLeft:SetPoint('BOTTOMLEFT', StanceButton1, 'BOTTOMLEFT', -11, -3)
-
-SlidingActionBarTexture0:ClearAllPoints()
-SlidingActionBarTexture0:SetPoint('LEFT', PetActionButton1, 'LEFT', -36, 1)
-
-MainMenuExpBar:ClearAllPoints()
-MainMenuExpBar:SetPoint('BOTTOM', MainMenuBar, 'BOTTOM')
+local Background = select(2, MainMenuExpBar:GetRegions())
+Background:SetTexture([[Interface\Buttons\WHITE8X8]])
+Background:SetVertexColor(0, 0, 0, 0.8)
 
 local MainMenuBarArtLarge = MainMenuBarArtFrame:CreateTexture(nil, 'BACKGROUND')
 MainMenuBarArtLarge:SetAtlas('hud-MainMenuBar-large', true)
@@ -89,12 +81,12 @@ MainMenuBarArtSmall:SetAtlas('hud-MainMenuBar-small', true)
 MainMenuBarArtSmall:SetPoint('BOTTOM')
 
 local ExpBarArtLarge = MainMenuExpBar:CreateTexture(nil, 'OVERLAY')
-ExpBarArtLarge:SetAtlas('hud-MainMenuBar-experiencebar-large-single', true)
-ExpBarArtLarge:SetPoint('CENTER')
+ExpBarArtLarge:SetAtlas('hud-MainMenuBar-experiencebar-large-single')
+ExpBarArtLarge:SetAllPoints(true)
 
 local ExpBarArtSmall = MainMenuExpBar:CreateTexture(nil, 'OVERLAY')
-ExpBarArtSmall:SetAtlas('hud-MainMenuBar-experiencebar-small-single', true)
-ExpBarArtSmall:SetPoint('CENTER')
+ExpBarArtSmall:SetAtlas('hud-MainMenuBar-experiencebar-small-single')
+ExpBarArtSmall:SetAllPoints(true)
 
 local ReputationBarArtLarge = ReputationWatchBar.StatusBar:CreateTexture(nil, 'OVERLAY')
 ReputationBarArtLarge:SetAtlas('hud-MainMenuBar-experiencebar-large-single')
@@ -104,18 +96,35 @@ local ReputationBarArtSmall = ReputationWatchBar.StatusBar:CreateTexture(nil, 'O
 ReputationBarArtSmall:SetAtlas('hud-MainMenuBar-experiencebar-small-single')
 ReputationBarArtSmall:SetAllPoints(true)
 
-local ReputationWatchBarDelegate = CreateFrame('Frame', nil, MainMenuBar)
+local ReputationWatchBarDelegate = CreateFrame('Frame', nil, ReputationWatchBar)
 ReputationWatchBarDelegate:SetHeight(13)
-print(ReputationWatchBarDelegate)
 
-local function UpdateWidth(width)
-    -- MainMenuExpBar:SetWidth(width)
-    -- ReputationWatchBar:SetWidth(width)
-    ReputationWatchBarDelegate:SetWidth(width)
-end
+ReputationWatchBar:EnableMouse(false)
+ReputationWatchBar.StatusBar:EnableMouse(false)
+ReputationWatchBar.StatusBar:SetParent(ReputationWatchBarDelegate)
+ReputationWatchBar.StatusBar:ClearAllPoints()
+ReputationWatchBar.StatusBar:SetAllPoints(true)
+ReputationWatchBar.StatusBar.Background:SetTexture([[Interface\Buttons\WHITE8X8]])
+ReputationWatchBar.StatusBar.Background:SetVertexColor(0, 0, 0, 0.8)
+ReputationWatchBar.OverlayFrame:SetParent(ReputationWatchBarDelegate)
+ReputationWatchBar.OverlayFrame:ClearAllPoints()
+ReputationWatchBar.OverlayFrame:SetAllPoints(ReputationWatchBar.StatusBar)
+ReputationWatchBar.OverlayFrame:EnableMouse(false)
 
-local function UpdateTexture(isLarge)
-    if isLarge then
+local OnEnter = ReputationWatchBar:GetScript('OnEnter')
+local OnLeave = ReputationWatchBar:GetScript('OnLeave')
+
+ReputationWatchBarDelegate:SetScript('OnEnter', function()
+    return OnEnter(ReputationWatchBar)
+end)
+ReputationWatchBarDelegate:SetScript('OnLeave', function()
+    return OnLeave(ReputationWatchBar)
+end)
+
+ns.securehook('MultiActionBar_Update', function()
+    if MultiBarBottomRight:IsShown() then
+        ReputationWatchBarDelegate:SetWidth(LARGE_WIDTH)
+
         MainMenuBarArtLarge:Show()
         MainMenuBarArtSmall:Hide()
 
@@ -125,6 +134,8 @@ local function UpdateTexture(isLarge)
         ReputationBarArtLarge:Show()
         ReputationBarArtSmall:Hide()
     else
+        ReputationWatchBarDelegate:SetWidth(SMALL_WIDTH)
+
         MainMenuBarArtLarge:Hide()
         MainMenuBarArtSmall:Show()
 
@@ -134,56 +145,15 @@ local function UpdateTexture(isLarge)
         ReputationBarArtLarge:Hide()
         ReputationBarArtSmall:Show()
     end
-end
-
-local LARGE_WIDTH = select(2, GetAtlasInfo('hud-MainMenuBar-large'))
-local SMALL_WIDTH = select(2, GetAtlasInfo('hud-MainMenuBar-small'))
-
-ReputationWatchBar:EnableMouse(false)
-ReputationWatchBar.StatusBar:EnableMouse(false)
-ReputationWatchBar.StatusBar:SetParent(ReputationWatchBarDelegate)
-ReputationWatchBar.StatusBar:ClearAllPoints()
-ReputationWatchBar.StatusBar:SetPoint('BOTTOMLEFT')
-ReputationWatchBar.StatusBar:SetPoint('BOTTOMRIGHT')
-ReputationWatchBar.OverlayFrame:SetParent(ReputationWatchBarDelegate)
-ReputationWatchBar.OverlayFrame:ClearAllPoints()
-ReputationWatchBar.OverlayFrame:SetAllPoints(ReputationWatchBar.StatusBar)
-ReputationWatchBar.OverlayFrame:EnableMouse(false)
-
-ReputationWatchBarDelegate:SetScript('OnEnter', function()
-    return ReputationWatchBar:GetScript('OnEnter')(ReputationWatchBar)
-end)
-ReputationWatchBarDelegate:SetScript('OnLeave', function()
-    return ReputationWatchBar:GetScript('OnLeave')(ReputationWatchBar)
-end)
-
-ns.securehook(ReputationWatchBar, 'Hide', function()
-    ReputationWatchBarDelegate:Hide()
-end)
-
-ns.securehook(ReputationWatchBar, 'Show', function()
-    ReputationWatchBarDelegate:Show()
-end)
-
-ns.securehook('MultiActionBar_Update', function()
-    local isLarge = MultiBarBottomRight:IsShown()
-    UpdateWidth(isLarge and LARGE_WIDTH or SMALL_WIDTH)
-    UpdateTexture(isLarge)
 end)
 
 ns.securehook('MainMenuTrackingBar_Configure', function(ReputationWatchBar)
-    if MainMenuExpBar:IsShown() then
-        ReputationWatchBarDelegate:ClearAllPoints()
-        ReputationWatchBarDelegate:SetPoint('BOTTOM', MainMenuBar, 'BOTTOM', 0, MainMenuExpBar:GetHeight())
-    else
-        ReputationWatchBarDelegate:ClearAllPoints()
-        ReputationWatchBarDelegate:SetPoint('BOTTOM', MainMenuBar, 'BOTTOM', 0, 0)
-    end
+    ReputationWatchBar.OverlayFrame.Text:SetPoint('CENTER')
+    ReputationWatchBarDelegate:SetPoint('BOTTOM', MainMenuExpBar, 'BOTTOM', 0, MainMenuExpBar:IsShown() and 10 or 0)
 end)
 
 ns.securehook('MainMenuBarVehicleLeaveButton_Update', function()
     local button = MainMenuBarVehicleLeaveButton
-    -- button:Show()
     if not button:IsShown() then
         return
     end
@@ -194,12 +164,22 @@ ns.securehook('MainMenuBarVehicleLeaveButton_Update', function()
     end
 end)
 
-local function SetupShowHide(frame, onShowHide, refs)
-    local handle = CreateFrame('Frame', nil, frame, 'SecureHandlerShowHideTemplate')
-    handle:SetAttribute('_onshow', onShowHide)
-    handle:SetAttribute('_onhide', onShowHide)
+local Ref = CreateFrame('Frame', nil, UIParent, 'SecureHandlerBaseTemplate')
+do
+    local Frames = {
+        PetActionButton1,
+        StanceButton1,
+        ActionButton1,
+        MultiBarBottomLeft,
+        MultiBarBottomRight,
+        MainMenuBar,
+        MainMenuExpBar,
+        ReputationWatchBar,
+        MainMenuBarArtFrame,
+        ReputationWatchBarDelegate = ReputationWatchBarDelegate,
+    }
 
-    for k, v in pairs(refs) do
+    for k, v in pairs(Frames) do
         local name
         if type(k) == 'number' then
             name = v:GetName()
@@ -208,8 +188,15 @@ local function SetupShowHide(frame, onShowHide, refs)
         end
 
         assert(name)
-        handle:SetFrameRef(name, v)
+        Ref:SetFrameRef(name, v)
     end
+end
+
+local function SetupShowHide(frame, onShowHide)
+    local handle = CreateFrame('Frame', nil, frame, 'SecureHandlerShowHideTemplate')
+    handle:SetAttribute('_onshow', onShowHide)
+    handle:SetAttribute('_onhide', onShowHide)
+    handle:SetFrameRef('ref', Ref)
 
     ns.runattribute(handle, '_onshow')
 
@@ -217,9 +204,10 @@ local function SetupShowHide(frame, onShowHide, refs)
 end
 
 local LayoutMainMenuBar = format([[
-local MultiBarBottomRight = self:GetFrameRef('MultiBarBottomRight')
-local MainMenuExpBar = self:GetFrameRef('MainMenuExpBar')
-local MainMenuBar = self:GetFrameRef('MainMenuBar')
+local ref = self:GetFrameRef('ref')
+local MultiBarBottomRight = ref:GetFrameRef('MultiBarBottomRight')
+local MainMenuExpBar = ref:GetFrameRef('MainMenuExpBar')
+local MainMenuBar = ref:GetFrameRef('MainMenuBar')
 
 local width = MultiBarBottomRight:IsShown() and %d or %d
 
@@ -228,57 +216,57 @@ MainMenuExpBar:SetWidth(width)
 ]], LARGE_WIDTH, SMALL_WIDTH)
 
 local LayoutWatchBars = [[
-local MainMenuExpBar = self:GetFrameRef('MainMenuExpBar')
-local ReputationWatchBar = self:GetFrameRef('ReputationWatchBar')
-local MainMenuBarArtFrame = self:GetFrameRef('MainMenuBarArtFrame')
-local MultiBarBottomLeft = self:GetFrameRef('MultiBarBottomLeft')
+local ref = self:GetFrameRef('ref')
+local MainMenuExpBar = ref:GetFrameRef('MainMenuExpBar')
+local ReputationWatchBar = ref:GetFrameRef('ReputationWatchBar')
+local MainMenuBarArtFrame = ref:GetFrameRef('MainMenuBarArtFrame')
+local MultiBarBottomLeft = ref:GetFrameRef('MultiBarBottomLeft')
+local ReputationWatchBarDelegate = ref:GetFrameRef('ReputationWatchBarDelegate')
 
 local y = 0
 
-local expBar = MainMenuExpBar:IsShown()
-local repBar = ReputationWatchBar:IsShown()
+local expBarShown = MainMenuExpBar:IsShown()
+local repBarShown = ReputationWatchBar:IsShown()
 
-if repBar then
-    if not expBar then
-        y = y + 13
-    else
-        y = y + 8
-    end
+MainMenuExpBar:SetHeight(repBarShown and 10 or 13)
+ReputationWatchBarDelegate:SetHeight(expBarShown and 10 or 13)
+
+if expBarShown then
+    y = y + MainMenuExpBar:GetHeight()
 end
 
-if expBar then
-    y = y + MainMenuExpBar:GetHeight()
+if repBarShown then
+    y = y + ReputationWatchBarDelegate:GetHeight()
 end
 
 MainMenuBarArtFrame:SetPoint('BOTTOM', '$parent', 'BOTTOM', 0, y)
 ]]
 
 local LayoutStanceBar = [[
-local StanceButton1 = self:GetFrameRef('StanceButton1')
-local ActionButton1 = self:GetFrameRef('ActionButton1')
-local MultiBarBottomLeft = self:GetFrameRef('MultiBarBottomLeft')
+local ref = self:GetFrameRef('ref')
+local StanceButton1 = ref:GetFrameRef('StanceButton1')
+local ActionButton1 = ref:GetFrameRef('ActionButton1')
+local MultiBarBottomLeft = ref:GetFrameRef('MultiBarBottomLeft')
 
 StanceButton1:ClearAllPoints()
 StanceButton1:SetPoint('BOTTOMLEFT', ActionButton1, 'TOPLEFT', 33, MultiBarBottomLeft:IsShown() and 54 or 11)
 ]]
 
 local LayoutPetActionBar = [[
-local PetActionButton1 = self:GetFrameRef('PetActionButton1')
-local ActionButton1 = self:GetFrameRef('ActionButton1')
-local MultiBarBottomLeft = self:GetFrameRef('MultiBarBottomLeft')
+local ref = self:GetFrameRef('ref')
+local PetActionButton1 = ref:GetFrameRef('PetActionButton1')
+local ActionButton1 = ref:GetFrameRef('ActionButton1')
+local MultiBarBottomLeft = ref:GetFrameRef('MultiBarBottomLeft')
 
 PetActionButton1:ClearAllPoints()
 PetActionButton1:SetPoint('BOTTOMLEFT', ActionButton1, 'TOPLEFT', 68, MultiBarBottomLeft:IsShown() and 54 or 11)
 ]]
 
-SetupShowHide(MultiBarBottomLeft, LayoutStanceBar .. LayoutPetActionBar,
-              {PetActionButton1, StanceButton1, ActionButton1, MultiBarBottomLeft})
-SetupShowHide(MultiBarBottomRight, LayoutMainMenuBar, {MultiBarBottomRight, MainMenuBar, MainMenuExpBar})
-SetupShowHide(MainMenuExpBar, LayoutWatchBars,
-              {ReputationWatchBar, MainMenuExpBar, MainMenuBarArtFrame, MultiBarBottomLeft})
-SetupShowHide(ReputationWatchBar, LayoutWatchBars,
-              {ReputationWatchBar, MainMenuExpBar, MainMenuBarArtFrame, MultiBarBottomLeft})
-SetupShowHide(PetActionBarFrame, LayoutPetActionBar, {PetActionButton1, ActionButton1, MultiBarBottomLeft})
+SetupShowHide(MultiBarBottomLeft, LayoutStanceBar .. LayoutPetActionBar)
+SetupShowHide(MultiBarBottomRight, LayoutMainMenuBar)
+SetupShowHide(MainMenuExpBar, LayoutWatchBars)
+SetupShowHide(ReputationWatchBar, LayoutWatchBars)
+SetupShowHide(PetActionBarFrame, LayoutPetActionBar)
 
 do -- bags and micro buttons
     local count = 8
--- CVars.lua
+++ CVars.lua
@@ -11,9 +11,17 @@ ns.login(function()
     ConsoleExec('SET portal TW')
     SetCVar('profanityFilter', false)
     -- @end-debug@
+
     SetCVar('alwaysCompareItems', false)
+    SetCVar('xpBarText', true)
+
     SetCVar('nameplateLargerScale', 1.2)
+    SetCVar('nameplateLargeTopInset', 0.1)
+    SetCVar('nameplateMinAlpha', 1.0)
+    SetCVar('nameplateMinAlphaDistance', 10)
     SetCVar('nameplateMotionSpeed', 0.025)
     SetCVar('nameplateNotSelectedAlpha', 0.5)
-    SetCVar('xpBarText', true)
+    SetCVar('nameplateOtherTopInset', 0.08)
+    SetCVar('nameplatePersonalHideDelaySeconds', 3.0)
+    SetCVar('nameplateSelectedScale', 1)
 end)
