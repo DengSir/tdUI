46a51b5f0476a66ffa99b1c9e2edc1d22fe53f51
Dencer <tdaddon@163.com>
2020-07-27 17:20:12 +0800

    update

---
+++ Map/NovaWorldBuffs.lua
@@ -0,0 +1,21 @@
+-- NovaWorldBuffs.lua
+-- @Author : Dencer (tdaddon@163.com)
+-- @Link   : https://dengsir.github.io
+-- @Date   : 7/27/2020, 4:29:35 PM
+---@type ns
+local ns = select(2, ...)
+
+ns.addon('NovaWorldBuffs', function()
+    MinimapLayerFrameLeft:Hide()
+    MinimapLayerFrameRight:Hide()
+    MinimapLayerFrameMiddle:Hide()
+
+    MinimapLayerFrame:SetScript('OnEnter', nil)
+    MinimapLayerFrame:SetScript('OnLeave', nil)
+
+    MinimapLayerFrameFS:SetFont(STANDARD_TEXT_FONT, 13, 'OUTLINE')
+    MinimapLayerFrameFS.SetFont = nop
+
+    NWB:setMmColor(nil, 1, 0.82, 0)
+    NWB.options.args.mmColor.disabled = true
+end)
