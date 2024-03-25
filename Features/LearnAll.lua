3f7d4e1b656f62be8a12728b5676e3719c6c01d9
Jai <814683@qq.com>
2024-03-26 00:10:17 +0800

    feat: 技能训练框添加全部训练按钮

---
+++ Features/LearnAll.lua
@@ -0,0 +1,22 @@
+---@type ns
+local ns = select(2, ...)
+
+ns.addon('Blizzard_TrainerUI', function()
+    local button = CreateFrame('Button', nil, ClassTrainerFrame, 'UIPanelButtonTemplate')
+    button:SetSize(80, 22)
+    button:SetPoint('CENTER', ClassTrainerFrame, 'TOPLEFT', 57, -420)
+    button:SetText('全部训练')
+    button:Show()
+    button:SetScript('OnClick', function()
+        for i = GetNumTrainerServices(), 1, -1 do
+            local _, _, serviceType = GetTrainerServiceInfo(i)
+            if serviceType == 'available' then
+                ClassTrainer_SetSelection(i)
+                if GetTrainerServiceCost(i) <= GetMoney() then
+                    BuyTrainerService(i)
+                end
+            end
+        end
+        ClassTrainerFrame_Update()
+    end)
+end)
