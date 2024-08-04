14149fbf4014ae29e2973ae85dd4a1339d3259d5
Jai <814683@qq.com>
2024-08-05 00:09:46 +0800

    refactor: 钓鱼改用交互按键

--- Features/Fishing.lua
+++ Features/Fishing.lua
@@ -7,15 +7,25 @@
 local ns = select(2, ...)
 
 -- ns.login(function()
-    local Action = CreateFrame('Frame', 'tdFishingButton', UIParent, 'SecureHandlerStateTemplate')
+local Action = CreateFrame('Frame', 'tdFishingButton', UIParent, 'SecureHandlerStateTemplate')
 
-    Action:SetAttribute('_onstate-usable', [[
+Action:SetAttribute('_onstate-usable', [[
         if newstate == 1 then
             self:SetBindingSpell(true, '1', '钓鱼')
-            self:SetBinding(true, '2', 'INTERACTMOUSEOVER')
+            self:SetBinding(true, '2', 'INTERACTTARGET')
         else
             self:ClearBindings()
         end
     ]])
-    RegisterStateDriver(Action, 'usable', '[equipped:鱼竿]1;0')
+Action:HookScript('OnAttributeChanged', function(_, _, state)
+    if InCombatLockdown() then
+        return
+    end
+    if state == 1 then
+        SetCVar('SoftTargetInteract', 3)
+    else
+        SetCVar('SoftTargetInteract', 1)
+    end
+end)
+RegisterStateDriver(Action, 'usable', '[equipped:鱼竿]1;0')
 -- end)
