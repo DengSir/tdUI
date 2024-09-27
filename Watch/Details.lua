1bbe011fe362b50b5e58af56ba90b43eeabed2a0
Jai <814683@qq.com>
2024-09-27 22:55:51 +0800

    feat: Detals

--- Watch/Details.lua
+++ Watch/Details.lua
@@ -2,7 +2,7 @@
 local ns = select(2, ...)
 
 
-ns.addonlogin('Details', function()
+ns.addonlogin('Details_TWW', function()
     local Details = _G.Details
 
     Details:ReabrirTodasInstancias()
@@ -12,14 +12,27 @@ ns.addonlogin('Details', function()
         return
     end
 
-    ns.WatchManager:Register(instance.baseframe, 2, {     --
+    ns.WatchManager:Register(instance.baseframe, 2, { --
         header = instance.baseframe.titleBar,
         marginLeft = 5,
         marginRight = 10,
         marginBottom = 5,
-        marginTop = 20,
+        marginTop = 40,
     })
 
-    instance:ChangeSkin("Forced Square")
+    local function update()
+        local c = math.min(instance.rows_showing, 5)
+        local h = math.max(1, c * instance.row_height)
+        instance.baseframe:SetHeight(h)
+    end
+
+    ns.hook(Details, 'RefreshMainWindow', function(orig, ...)
+        if instance.baseframe:IsVisible() then
+            orig(...)
+            update()
+        end
+    end)
+
+    instance:ChangeSkin("|cff8080ffThe War Within|r")
     instance:LockInstance(true)
 end)
